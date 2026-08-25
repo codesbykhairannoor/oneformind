6623f02939b7c4b239138cc206bba01a18084718
Khairan Aja <khairking6@gmail.com>
2026-08-25 16:58:52 +0700

    fix: Remove studio from prisma config for TS error

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,8 +4,5 @@ import 'dotenv/config'; // Ensure .env is loaded
 export default defineConfig({
   datasource: {
     url: process.env.DATABASE_URL
-  },
-  studio: {
-    url: process.env.DATABASE_URL
   }
 });
