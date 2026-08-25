95ed92b7f71d0086d4bba9c55d1759aadc95e60b
Khairan Aja <khairking6@gmail.com>
2026-08-25 18:06:17 +0700

    fix: Allow Google OAuth to link with existing Laravel user accounts

--- src/auth.ts
+++ src/auth.ts
@@ -13,6 +13,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: "Credentials",
