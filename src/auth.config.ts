d76aecd057aa19410b0018c23828d7a2ea1385ba
Khairan Aja <khairking6@gmail.com>
2026-08-25 20:33:49 +0700

    fix(auth): add trustHost and secret fallback to prevent ENVIRONMENT_FALLBACK

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,8 @@
 import type { NextAuthConfig } from "next-auth"
 
 export const authConfig = {
+  secret: process.env.AUTH_SECRET || "ea22ef5cde1e1d9932eb2d6cca05e0950986dcd0322a246d2bfc19ce2c9a6618",
+  trustHost: true,
   session: { strategy: "jwt" },
   pages: {
     signIn: '/login',
